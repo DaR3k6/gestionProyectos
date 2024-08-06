a3ab2134d57290184e7d4077aa98d34fdd1c4216
DaR3k6 <kevinvilleperez@gmail.com>
2024-08-06 18:02:22 -0500

    Corrigiendo el codigo largo

--- src/infrastructure/controller/UserController.ts
+++ src/infrastructure/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { UserCase } from "../../app/UserCase";
+import { Expression } from "mongoose";
 
 export class UserController {
   constructor(private readonly userUseCase: UserCase) {}
@@ -18,39 +19,31 @@ export class UserController {
           .status(400)
           .send({ mensaje: "Usuario no encontrado", status: false });
       }
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).send({
-          error: `Error en la base de datos: ${error.message}`,
-          status: false,
-        });
-      } else {
-        res.status(500).send({
-          error: "Error desconocido en la base de datos",
-          status: false,
-        });
-      }
+    } catch (error: any) {
+      res.status(500).send({
+        error: `Error en la base de datos: ${error.message}`,
+        status: false,
+      });
     }
   };
 
   public insertController = async ({ body }: Request, res: Response) => {
     try {
+      if (!body) {
+        res
+          .status(404)
+          .send({ mensaje: "Los campos estan vacios", status: false });
+      }
+
       const user = await this.userUseCase.registerUser(body);
       res
         .status(200)
         .send({ mensaje: "Registro de un usuario", user, status: true });
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).send({
-          error: `Error en la base de datos: ${error.message}`,
-          status: false,
-        });
-      } else {
-        res.status(500).send({
-          error: "Error desconocido en la base de datos",
-          status: false,
-        });
-      }
+    } catch (error: any) {
+      res.status(500).send({
+        error: `Error en la base de datos: ${error.message}`,
+        status: false,
+      });
     }
   };
 
@@ -60,18 +53,11 @@ export class UserController {
       res
         .status(200)
         .send({ mensaje: "Traer todos los usuarios", user, status: true });
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).send({
-          error: `Error en la base de datos: ${error.message}`,
-          status: false,
-        });
-      } else {
-        res.status(500).send({
-          error: "Error desconocido en la base de datos",
-          status: false,
-        });
-      }
+    } catch (error: any) {
+      res.status(500).send({
+        error: `Error en la base de datos: ${error.message}`,
+        status: false,
+      });
     }
   };
 }
